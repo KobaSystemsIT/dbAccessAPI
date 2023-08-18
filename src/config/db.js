1a9aa9fa1984ae149683d6bdb3413552fbffa583
Alfredo Bautista <abautista200214@gmail.com>
2023-08-18 16:06:17 -0600

    changes in db

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const dataSQL = mysql.createPool({
     host: process.env.HOST,
-    user: 'process.env.USER',
+    user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.NAME
 });
