96d41d398af5cbded84daeecf66fe65ed6662f3a
Alfredo Bautista <abautista200214@gmail.com>
2023-08-18 03:24:59 -0600

    dbAccessApi

---
+++ src/models/user.js
@@ -0,0 +1,37 @@
+const db = require('../config/db');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // Número de rondas de encriptación
+
+async function getUserByUsername(username) {
+    const [rows] = await db.query('SELECT * FROM adminUser WHERE username = ?', [username]);
+    if (rows.length === 0) {
+      return null; // Usuario no encontrado
+    }
+    return rows[0];
+  }
+  
+
+async function registerUser(username, password) {
+    try{
+        async function hashPassword(password) {
+            const salt = await bcrypt.genSalt(saltRounds);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            return hashedPassword;
+          }
+        
+          const plainPassword = password; // Mueve esta línea fuera de la función asíncrona
+        
+          const hashedPassword = await hashPassword(plainPassword);
+        
+          const [rows] = await db.query('CALL addAdminUser(?, ?)', [username, hashedPassword]);
+          return rows;
+    } catch (error) {
+        console.error('Mysql: ', error);
+        throw error;
+    }
+  }
+  
+module.exports = {
+    getUserByUsername,
+    registerUser,
+}
