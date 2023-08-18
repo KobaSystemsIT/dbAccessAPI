942780333f103c3ddf36bda659fec35f6c9e0b4e
Alfredo Bautista <abautista200214@gmail.com>
2023-08-18 12:02:41 -0600

    CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const dbaccessapi = require('./routes/dbaccessapi');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
+app.enable('trust proxy');
+
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-host']) {
+        req.headers.host = req.headers['x-forwarded-host'];
+    }
+    next();
+});
 
 app.use(express.json());
 
